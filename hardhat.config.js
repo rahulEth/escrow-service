6953c6f2a1b2bdea701cbc05c617c63fb84b5185
rahuleth <rahulsaini593@gmail.com>
2024-11-12 02:34:58 +0530

    config file updted

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+
+// https://base-mainnet.g.alchemy.com/v2/9Gt8lpwu_WelXSIE83OLYtiB0Smb3CWS
+// sapolia
+const META_MASK_PRIVATE_KEY= process.env.APP_PRIVATE_KEY;
+const INFURA_API_KEY= process.env.INFURA_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  solidity: {
+    compilers:[
+      {
+        version: "0.8.25"
+      }
+    ]
+  },
+  networks:{
+    localhost:{
+      url: 'http://127.0.0.1:8545'
+    },
+    sepoliaTestnet:{
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+
+      accounts: [META_MASK_PRIVATE_KEY] ,
+    },
+
+  }
 };
